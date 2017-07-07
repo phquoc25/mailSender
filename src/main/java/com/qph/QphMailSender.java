3c74cd1a71167a74d8ddad184bbfa1219fc34ffa
Ho Quoc PHAN <phquoc25@gmail.com>
2017-07-07 04:59:35 +0200

    [qphmail-sender] sending email with html body

--- src/main/java/com/qph/QphMailSender.java
+++ src/main/java/com/qph/QphMailSender.java
@@ -7,10 +7,11 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by quoc on 21/06/2017.
@@ -32,16 +33,13 @@ public class QphMailSender {
     @Autowired
     private QphMailSenderProperties senderProperties;
 
-    public QphMailSender() {
-    }
-
     public void sendMail() throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
         mimeMessageHelper.setFrom(senderProperties.getSender());
         mimeMessageHelper.setTo(senderProperties.getReceivers());
         mimeMessageHelper.setSubject(senderProperties.getSubject());
-        mimeMessageHelper.setText(senderProperties.getBody());
+        mimeMessageHelper.setText(senderProperties.getBody(), true);
         mimeMessageHelper.addAttachment(senderProperties.getAttachmentFileName().replace(DATE_PART, TODAY),
                 new FileSystemResource(senderProperties.getAttachmentFilePath().replace(DATE_PART, TODAY)));
         this.mailSender.send(mimeMessage);
